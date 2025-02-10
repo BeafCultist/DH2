a37c2857e1c4483d331a9ea6496205a81d5fecd1
BeafCultist <147671982+BeafCultist@users.noreply.github.com>
2025-02-10 17:57:56 -0500

    natDexTier

--- data/mods/gen8paleomons/formats-data.ts
+++ data/mods/gen8paleomons/formats-data.ts
@@ -1,406 +1,406 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	kabutoancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	kabutopsancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	omanyteancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	omastarancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	aerodactylancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	anorithancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	armaldoancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	lileepancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	cradilyancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	torkoalpottery: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	relicanthscorched: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	swinubancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	piloswineancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	mamoswineancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	dodrumb: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	blossobite: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	ghoulipinch: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	ghoulpion: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	cranidoscretaceous: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	rampardoscretaceous: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	shieldonancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	bastiodonancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	tirtougaleatherback: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	carracostaleatherback: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	archenancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	archeopsancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	tyruntapex: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	tyrantrumapex: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	amauraregnant: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	aurorusregnant: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	shellosentity: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	gastrodonwestentity: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	gastrodoneastentity: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	yanmaancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	yanmegaancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	tangelaancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	tangrowthancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	liluǒ: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	flaruǒ: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	alohwo: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	wonkway: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	illusinogen: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	robusteel: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	velovolt: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	vishcaca: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	dracosaur: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	gorlifross: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	arctachoris: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	dreepyluminous: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	drakloakluminous: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	dragapultluminous: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	larvitarnature: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	pupitarnature: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	tyranitarnature: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	giblepersistent: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	gabitepersistent: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	garchomppersistent: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	scorcharnia: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	listoxina: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	spinollina: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	plusleprimal: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	minunprimal: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	swalotprimal: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	hariyamaprimal: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	grumpigprimal: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	vibravaclassical: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	flygonclassical: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	sphealancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	sealeoancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	walreinancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	whismurancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	loudredancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	exploudancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	anklarmor: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	drakabyssal: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	trobsidon: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	dhelmiseancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	honedgeancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	doubladeancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	aegislashancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	baltoypremade: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	claydolpremade: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	parasancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	parasectancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	parasinensis: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	girafarigancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	oligosogilo: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	poochyenaancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	mightyenaancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	matriaryena: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	teenizino: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	terrorzino: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	sailad: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	pharaocious: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	honkeri: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	melophus: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	noibatancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	noivernancient: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	dianciecataclysm: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	onixcrystal: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 	steelixcrystal: {
-		tier: "Paleomons",
+		natDexTier: "Paleomons",
 		doublesTier: "Paleomons Doubles",
 	},
 
